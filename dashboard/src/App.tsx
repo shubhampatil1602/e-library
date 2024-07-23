92e4086fc6e48a6368a22cffde4b667954859525
Shubham Patil <shubhamspatilnbr@gmail.com>
2024-07-23 10:56:30 +0530

    add project setup

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      <Button variant={'link'}>Add</Button>
+    </div>
+  );
+};
+
+export default App;
